afa2ce42794aa77c0538031398e00d69acaa24de
Mehedi Hassan Jibon <mhj124177@gmail.com>
2022-05-24 18:45:57 +0600

    inventory detail page added

--- index.js
+++ index.js
@@ -26,6 +26,44 @@ async function run() {
         const result = await cursor.toArray();
         res.send(result);
     })
+    app.get('/myitems', async (req, res) => {
+        const email = req.query.email;
+        const query = { email: email };
+        const cursor = laptopCollection.find(query);
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+    app.get('/inventory/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) }
+        const result = await laptopCollection.findOne(query);
+        res.send(result);
+    })
+    app.put('/inventory/:id', async (req, res) => {
+        const id = req.params.id;
+        const quantity = req.body.quantity;
+        const deliveryQuantity = req.body.delivery;
+        const updateInput = req.body.updateInput;
+        const filter = { _id: ObjectId(id) };
+        const options = { upsert: true };
+        let updateDoc;
+        if (deliveryQuantity === 'delivered') {
+            updateDoc = {
+                $set: {
+                    quantity: quantity - 1
+                },
+            };
+        }
+        else {
+            updateDoc = {
+                $set: {
+                    quantity: quantity + updateInput
+                },
+            };
+        }
+        const result = await laptopCollection.updateOne(filter, updateDoc, options);
+        res.send(result.acknowledged);
+    })
     app.delete('/manage', async (req, res) => {
         const id = req.query.id;
         const query = { _id: ObjectId(id) }
